ba256314fef0e66a2fb91aa362cbe97e5c494985
matthewmt1998 <matthewtaylor98.mt@gmail.com>
2021-01-26 00:12:48 +0000

    move from app to toolbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Matthew Taylor | Software Developer';
-}
+export class AppComponent {}
