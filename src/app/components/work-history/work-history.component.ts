e8e7e84e10a2eb4ed35a0a7166259db42b9e8b06
matthewmt1998 <matthewtaylor98.mt@gmail.com>
2021-03-26 22:04:41 +0000

    reduce duplication of html

--- src/app/components/work-history/work-history.component.ts
+++ src/app/components/work-history/work-history.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'work-history',
   templateUrl: './work-history.component.html',
   styleUrls: ['./work-history.component.css'],
   providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false }
   }]
 })
 export class WorkHistoryComponent implements OnInit {
@@ -17,13 +17,20 @@ export class WorkHistoryComponent implements OnInit {
   }
 
   title = "Work History"
+  steppericons = STEPPERICONS
   jobs = JOBS
-  
+
 
 }
 
+const STEPPERICONS = [
+  { stepperStatus: "number", icon: 'work' },
+  { stepperStatus: "edit", icon: 'work_outline' },
+  { stepperStatus: "done", icon: 'work_outline' }
+]
+
 const JOBS = [
-  {timeInJobAndJobTitle: "March 2021-Present - Software Engineer", content:'Superman', img: "assets/img/waterslogo.png"},
-  {timeInJobAndJobTitle: "Jan 2019-March 2021 - Junior Software Engineer", content:'Batman', img: "assets/img/waterslogo.png"},
-  {timeInJobAndJobTitle: "Sep 2017-Jan 2019 - Apprentice Software Engineer", content:'BatGirl', img: "assets/img/mdlogo.png"},
+  { timeInJobAndJobTitle: "March 2021-Present - Software Engineer", content: 'Superman', img: "assets/img/waterslogo.png" },
+  { timeInJobAndJobTitle: "Jan 2019-March 2021 - Junior Software Engineer", content: 'Batman', img: "assets/img/waterslogo.png" },
+  { timeInJobAndJobTitle: "Sep 2017-Jan 2019 - Apprentice Software Engineer", content: 'BatGirl', img: "assets/img/mdlogo.png" }
 ];
