272124f46da4ee3e69e8644954456b2fab0eb4da
matthewmt1998 <matthewtaylor98.mt@gmail.com>
2021-03-26 19:26:51 +0000

    update contact-me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppRoot } from './components/app-root/app.root';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -19,6 +21,9 @@ import { ContactMeComponent } from './components/contact-me/contact-me.component
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
