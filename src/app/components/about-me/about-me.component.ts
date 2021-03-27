b51a13d9d7ec585e9d6c3cf9b05319d3b1890372
matthewmt1998 <matthewtaylor98.mt@gmail.com>
2021-03-27 01:59:48 +0000

    update about me

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LANGUAGES } from './languages';
 
 @Component({
   selector: 'about-me',
@@ -13,5 +14,7 @@ export class AboutMeComponent implements OnInit {
   }
 
   title = "About me"
-  content = "Hi! I am Matthew, a Software Engineer based in the North of England. I enjoy creating things, whether that be in the frontend or the backend. My interest in development began when I started to try to modify game files. From there I went on to a Level 4 apprenticeship in software development where I got great experience dealing with both emerging and legacy technology!"
+  intro = "Hi! I am Matthew, a Software Engineer based in the North of England. I enjoy creating things, whether that be in the frontend or the backend. My interest in development began when I started to try to modify game files. From there I went on to a Level 4 apprenticeship in software development where I got great experience dealing with both emerging and legacy technology!"
+  languageIntro = "Fast-forward today and I have had the privilege to work on many different technology stacks, Here are a few technologies I’ve been working with recently:"
+  languages = LANGUAGES
 }
